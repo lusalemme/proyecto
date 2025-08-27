99eae497aabc8c4ff81900aed3247c12fc3b481e
lucia <salemmelu@gmail.com>
2025-08-27 19:08:39 -0300

    Se agregó un buscador

--- inicio/views.py
+++ inicio/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from inicio.models import Alumno
-from inicio.forms import FormularioCargarAlumno
+from inicio.forms import FormularioCargarAlumno, FormularioBuscarAlumno
 
 def inicio(request):
     return render(request, 'inicio.html')
@@ -25,5 +25,12 @@ def cargar_alumno(request):
     return render(request, 'cargar_alumno.html', {'formulario': formulario})
 
 def listado_de_alumnos(request):
-    alumnos = Alumno.objects.all()
-    return render(request, 'listado_de_alumnos.html', {'alumnos': alumnos})
+    formulario = FormularioBuscarAlumno(request.GET)
+    if formulario.is_valid():
+        nombre_a_buscar = formulario.cleaned_data['nombre']
+        #nota_a_buscar = formulario.cleaned_data['nota']
+        alumnos_buscados = Alumno.objects.filter(nombre__icontains=nombre_a_buscar)
+        #alumnos_buscados = Alumno.objects.filter(nombre__icontains=nombre_a_buscar, nota=nota_a_buscar)
+
+
+    return render(request, 'listado_de_alumnos.html', {'alumnos_buscados': alumnos_buscados, 'formulario': formulario})
